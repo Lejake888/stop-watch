27d2990b6301bab9ab7a0c828e5003408519733e
Jake <lejake888@gmail.com>
2020-03-13 11:19:03 +0000

    Added some timer logic, toggles on and off

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-alert("Hello")
+class StopWatch  {
+    constructor() {
+        this.timing = false;
+    }
+}
+
+startTimer = () => {
+    console.log("Start");
+    timer.timing = true;
+}
+
+stopTimer = () => {
+    console.log("Finished");
+    timer.timing = false;
+}
+
+let timer = new StopWatch()
+let startButton = document.getElementById("start") 
+
+startButton.addEventListener("click", () => {
+    if (!timer.timing) {
+        startTimer()
+    }
+    else if (timer.timing) {
+        stopTimer()
+    }
+});
