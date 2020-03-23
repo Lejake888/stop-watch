02caf5f92f012487185981aea30eaa63160ddd74
Jake <lejake888@gmail.com>
2020-03-23 14:46:08 +0000

    Counter inside of console

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 let time = 0;
+let beginClock = null;
+let stopClock = null;
+let stopped = 0;
 let interval;
 
 class StopWatch  {
@@ -26,25 +29,56 @@ startTimer = () => {
     setTarget()
     console.log("Start");
     timer.timing = true;
+    if (beginClock === null) {
+        beginClock = new Date();
+    }
+
+    if (stopClock !== null) {
+        stopped += (new Date() - stopClock);
+    }
+    console.log(stopped);
+
+    started = setInterval(clock, 10);	
 }
 
 stopTimer = () => {
     console.log("Stop");
     timer.timing = false;
     startButton.disabled = true
+    resetButton.disabled = false
+    stopClock = new Date();
+    clearInterval(started);
 }
 
 resetTimer = () => {
     console.log("Reset");
     time = 0;
+    beginClock = null;
+    stopClock = null;
+    stopped = 0;
+    interval;
     timer.timing = false;
     startButton.disabled = false
+    resetButton.disabled = true
+}
+
+clock = () => {
+    let currentTime = new Date()
+    let timeTaken = new Date(currentTime - beginClock - stopped)
+    minutes = timeTaken.getUTCMinutes()
+    seconds = timeTaken.getUTCSeconds()
+    milliseconds = timeTaken.getUTCMilliseconds()
+    console.log(minutes)
+    console.log(seconds)
+    console.log(milliseconds)
 }
 
 let timer = new StopWatch()
 let startButton = document.getElementById("start") 
 let resetButton = document.getElementById("reset") 
 let target = document.getElementById("random") 
+startButton.disabled = false
+resetButton.disabled = true
 
 startButton.addEventListener("click", () => {
     if (!timer.timing) {
